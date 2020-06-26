6b4d06c10a7d067d773e3f4da86f15b6e513fa88
Jyot Sarup <52942229+jyotSarup@users.noreply.github.com>
2020-06-25 18:27:05 -0700

    added house issue route

--- src/router/routes.js
+++ src/router/routes.js
@@ -53,15 +53,19 @@ const routes = [
         name: "help.faqs",
         path: "/help/faqs",
         component: () => import("pages/Faqs.vue")
+    },
+    {
+        path:'/add-house-issue',
+        component:() => import('../pages/forms/AddHouseIssue.vue')
     }
-];
+]
 
 // Always leave this as last one
-if (process.env.MODE !== "ssr") {
+if (process.env.MODE !== 'ssr') {
     routes.push({
-        path: "*",
-        component: () => import("pages/Error404.vue")
-    });
+        path: '*',
+        component: () => import('pages/Error404.vue')
+    })
 }
 
-export default routes;
+export default routes
