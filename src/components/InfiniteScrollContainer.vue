cdf76e155e4892096669f57876a9b531e5647b4a
Jyot Sarup <52942229+jyotSarup@users.noreply.github.com>
2020-07-19 21:35:08 -0700

    homestay description

--- src/components/InfiniteScrollContainer.vue
+++ src/components/InfiniteScrollContainer.vue
@@ -3,7 +3,7 @@
         class="q-pa-md scroll"
         style=" padding: 0 ; padding-top: 4px; margin: 0 auto ; margin-top:16px;  border-radius:20px"
     >
-        <q-pull-to-refresh style="width:100%">
+        <q-pull-to-refresh style="width:100%" @refresh="refresher">
             <q-infinite-scroll v-if="this.items" @load="onLoad" :offset="100">
                 <FeedStart />
 
@@ -43,10 +43,7 @@ export default {
     },
     data() {
         return {
-            page: 2,
-            limit: 10,
             items: [],
-            total: 0,
             persistent: false,
             shortFeedContent: []
         };
@@ -54,84 +51,35 @@ export default {
 
     async created() {
         try {
-            await this.$store.dispatch(GET_FEED, {
-                householdid: parseInt(this.household.household.id),
-                limit: 10,
-                page: 1
-            });
-            await this.feeds.feed.items.forEach(item => {
-                this.items.push(item);
-            });
-
-            this.total = await this.feeds.feed.total;
-            console.log('total in created', this.total);
-            this.refresher(this.total);
+            this.refresher();
         } catch (error) {
             console.log(error);
         }
     },
 
-    // async mounted(){
-    //     console.log("mounted");
-    //     console.log("total", await this.total);
-    //             try {
-    //                 await this.$store.dispatch(GET_FEED, {
-    //                     householdid: parseInt(this.household.household.id),
-    //                     limit: this.total,
-    //                     page: 1
-    //                 });
-
-    //                 console.log('onload feed', this.feeds.feed);
-    //                 if (this.feeds.feed.items) {
-
-    //                     this.items = this.feeds.feed.items;
-    //                     this.total = this.feeds.feed.total;
-    //                     this.page++;
-
-    //                 }else{
-    //                 }
-    //             } catch (error) {
-    //                 console.log(error);
-    //             }
-
-    // },
+   
     methods: {
         async onLoad(index, done) {
             done();
         },
-        async refresher(total, done) {
-            console.log('total in refresher', total);
+        async refresher() {
             try {
                 await this.$store.dispatch(GET_FEED, {
                     householdid: parseInt(this.household.household.id),
-                    limit: total,
+                    limit: 10000,
                     page: 1
                 });
 
                 console.log('refresher feed', this.feeds.feed);
                 if (this.feeds.feed.items) {
                     this.items = this.feeds.feed.items;
-                    this.total = this.feeds.feed.total;
                 }
             } catch (error) {
                 console.log(error);
             }
-
-            // let items = []
-            // for (let i = 0; i < 7; i++) {
-            //   items.push({})
-            // }
-            // this.items = this.items.concat(items)
-            // done()
         },
 
         refresh(done) {
-            // setTimeout(() => {
-            //     if (this.items.length < 300) {
-            //         this.items.push({}, {}, {}, {}, {}, {}, {});
-            //         done();
-            //     }
-            // }, 1000);
         }
     }
 };
